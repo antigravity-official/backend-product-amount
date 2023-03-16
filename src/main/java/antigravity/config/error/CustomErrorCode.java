6cfa0c17898c7db14089c69bec59687b061fc556
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-03-16 23:19:44 +0900

    :sparkles: [feat] 금액에 대한 ErrorCode 추가

--- src/main/java/antigravity/config/error/CustomErrorCode.java
+++ src/main/java/antigravity/config/error/CustomErrorCode.java
@@ -12,6 +12,7 @@ public enum CustomErrorCode implements ErrorCode {
     FAIL_TO_USE_PROMOTION(3000, HttpStatus.OK, "해당 쿠폰을 사용할 수 없습니다."),
     INVALID_PROMOTION_EXPIRATION(3001, HttpStatus.OK, "해당 쿠폰의 유효기간이 만료되었습니다."),
     INVALID_PROMOTION_BEFORE_DATE(3002, HttpStatus.OK, "쿠폰 사용 기간이 아닙니다."),
+    INVALID_PROMOTION_PRICE(3002, HttpStatus.OK, "할인 금액이 상품 금액보다 큽니다."),
 
     // Server error : 4000
     INTERNAL_SERVER_ERROR(4000, HttpStatus.INTERNAL_SERVER_ERROR, "서버와의 연결에 실패했습니다."),
