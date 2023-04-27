6f2087040537e15c01d8c22364a37be12681281e
정휘영 <66720964+jhy950210@users.noreply.github.com>
2023-04-27 10:22:36 +0900

    Add 쿠폰 적용 상품 체크 메서드, 쿠폰 get 메서드 추가

--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package antigravity.repository;
 
 import antigravity.domain.entity.Product;
+import antigravity.domain.entity.Promotion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -27,4 +28,41 @@ public class ProductRepository {
                         .build()
         );
     }
+
+    public boolean isUsableCouponsToProduct(int productId, int[] promotionIds) {
+        String query = "SELECT COUNT(*) FROM `promotion_products` WHERE product_id = :productId AND promotion_id IN (:promotionIds) ";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("productId", productId);
+        params.addValue("couponIds", promotionIds);
+
+        int count = namedParameterJdbcTemplate.queryForObject(
+                query,
+                params,
+                Integer.class
+        );
+        return count == promotionIds.length;
+    }
+
+    public Promotion getPromotion(int promotionId) {
+        String query = "SELECT * FROM `promotion` WHERE id = :promotionId ";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("promotionId", promotionId);
+
+        return namedParameterJdbcTemplate.queryForObject(
+                query,
+                params,
+                (rs, rowNum) -> Promotion.builder()
+                        .id(rs.getInt("id"))
+                        .promotion_type(rs.getString("promotion_type"))
+                        .name(rs.getString("name"))
+                        .discount_type(rs.getString("discount_type"))
+                        .discount_value(rs.getInt("discount_value"))
+                        .use_started_at(rs.getDate("use_started_at"))
+                        .use_ended_at(rs.getDate("use_ended_at"))
+                        .build()
+        );
+    }
+
 }
