e1c780bb852f1503a21506055e5487e59d50bb7d
HeeseongKwon <hskwon@gentleenergycorp.com>
2023-04-30 16:18:33 +0900

    ProductAmountResponse id 필드 삭제

--- src/main/java/antigravity/model/response/product/ProductAmountResponse.java
+++ src/main/java/antigravity/model/response/product/ProductAmountResponse.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 @Getter
 @Builder
 public class ProductAmountResponse {
-	private Integer id; //상품 ID
 	private String name; //상품명
 	private int originPrice; //상품 기존 가격
 	private int discountPrice; //총 할인 금액
@@ -18,7 +17,6 @@ public class ProductAmountResponse {
 	//할인 적용 없는 경우
 	public static ProductAmountResponse toDto(final Product product) {
 		return ProductAmountResponse.builder()
-			.id(product.getId())
 			.name(product.getName())
 			.originPrice(product.getPrice())
 			.finalPrice(product.getPrice())
