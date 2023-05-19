e4f64b228f664297ece7bc88b5690b9c0389aa40
junheoyk95 <sos100izt@gmail.com>
2023-05-19 17:50:47 +0900

    데이터 추가

--- src/main/resources/hsqldb/data.sql
+++ src/main/resources/hsqldb/data.sql
@@ -2,12 +2,33 @@ INSERT INTO promotion
 VALUES (1, 'COUPON', '30000원 할인쿠폰', 'WON', 30000, '2022-11-01', '2023-03-01');
 INSERT INTO promotion
 VALUES (2, 'CODE', '15% 할인코드', 'PERCENT', 15, '2022-11-01', '2023-03-01');
+INSERT INTO promotion
+VALUES (3, 'COUPON', '10000원 할인쿠폰', 'WON', 10000, '2022-11-01', '2023-08-01');
+INSERT INTO promotion
+VALUES (4, 'CODE', '10% 할인코드', 'PERCENT', 10, '2022-11-01', '2023-08-01');
+INSERT INTO promotion
+VALUES (5, 'COUPON', '8000원 할인쿠폰', 'WON', 8000, '2022-11-01', '2023-08-01');
+INSERT INTO promotion
+VALUES (6, 'COUPON', '15000원 할인쿠폰', 'WON', 15000, '2023-07-01', '2023-08-01');
+INSERT INTO promotion
+VALUES (7, 'COUPON', '20000원 할인쿠폰', 'WON', 20000, '2023-01-01', '2023-02-01');
 
 INSERT INTO product
 VALUES (1, '피팅노드상품', 215000);
-
+INSERT INTO product
+VALUES (2, '티셔츠', 75000);
 
 INSERT INTO promotion_products
 VALUES (1, 1, 1);
 INSERT INTO promotion_products
 VALUES (2, 2, 1);
+INSERT INTO promotion_products
+VALUES (3, 3, 2);
+INSERT INTO promotion_products
+VALUES (4, 4, 2);
+INSERT INTO promotion_products
+VALUES (5, 5, 2);
+INSERT INTO promotion_products
+VALUES (6, 6, 2);
+INSERT INTO promotion_products
+VALUES (7, 7, 2);
