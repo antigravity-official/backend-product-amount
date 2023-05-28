3e4412faa40471940013ce4a888c80003708db7a
YUNMI LEE <goya3014@hanmail.net>
2023-05-28 15:01:08 +0900

    상품 가격조회 api update

--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package antigravity.repository;
 
 import antigravity.domain.entity.Product;
+import antigravity.domain.entity.Promotion;
+import antigravity.model.response.ProductAmountResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -24,6 +26,42 @@ public class ProductRepository {
                         .id(rs.getInt("id"))
                         .name(rs.getString("name"))
                         .price(rs.getInt("price"))
+                        .build()
+        );
+    }
+
+    public Promotion getPromotion(int couponId){
+
+        String query = "SELECT * FROM `PROMOTION` WHERE id = :id";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", couponId);
+
+        return namedParameterJdbcTemplate.queryForObject(
+                query,
+                params,
+                (rs, rowNum) -> Promotion.builder()
+                        .id(rs.getInt("id"))
+                        .promotion_type(rs.getString("promotion_type"))
+                        .name(rs.getString("name"))
+                        .discount_type(rs.getString("discount_type"))
+                        .discount_value(rs.getInt("discount_value"))
+                        .use_started_at(rs.getDate("use_started_at"))
+                        .use_ended_at(rs.getDate("use_ended_at"))
+                        .build()
+        );
+    }
+
+    public ProductAmountResponse getProductAmountResponse(int id){
+        String query = "SELECT * FROM `product` WHERE id = :id ";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
+
+        return namedParameterJdbcTemplate.queryForObject(
+                query,
+                params,
+                (rs, rowNum) -> ProductAmountResponse.builder()
+                        .name(rs.getString("name"))
+                        .originPrice(rs.getInt("price"))
                         .build()
         );
     }
--- src/main/java/antigravity/service/ProductService.java
+++ src/main/java/antigravity/service/ProductService.java
@@ -1,11 +1,12 @@
 package antigravity.service;
 
-import antigravity.domain.entity.Product;
+import antigravity.domain.entity.Promotion;
 import antigravity.model.request.ProductInfoRequest;
 import antigravity.model.response.ProductAmountResponse;
 import antigravity.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @Service
@@ -13,10 +14,48 @@ public class ProductService {
     private final ProductRepository repository;
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request) {
-        System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
 
-        Product product = repository.getProduct(request.getProductId());
+        ProductAmountResponse productAmountResponse = repository.getProductAmountResponse(request.getProductId());
 
-        return null;
-    }
+        int[] couponIds = request.getCouponIds();
+
+        Date nowDate = new Date();
+        int discountPrice = 0;
+
+        //쿠폰 유효성 체크 시작
+        for (int i = 0; i < couponIds.length; i++) {
+            Promotion promotion = repository.getPromotion(couponIds[i]);
+            //유효기간 체크
+            if((nowDate.equals(promotion.getUse_started_at())|| nowDate.after(promotion.getUse_started_at())) && (nowDate.equals(promotion.getUse_ended_at()) || nowDate.before(promotion.getUse_ended_at()))) {
+
+                //금액 할인
+                if(promotion.getPromotion_type().equals("COUPON")){
+                    discountPrice += promotion.getDiscount_value();
+                }else{//퍼센트 할인
+                    discountPrice += productAmountResponse.getOriginPrice() * promotion.getDiscount_value() / 100;
+                }
+
+            }
+        }//쿠폰 유효성 체크 종료
+
+        //할인금액 확정
+        productAmountResponse.setDiscountPrice(discountPrice);
+
+        //최소 상품가격: 10,000
+        if(productAmountResponse.getOriginPrice() - productAmountResponse.getDiscountPrice() < 10000){
+            productAmountResponse.setFinalPrice(10000);
+
+            //최대 상품가격 10,000,000
+        }else if(productAmountResponse.getOriginPrice() - productAmountResponse.getDiscountPrice() > 10000000){
+            productAmountResponse.setFinalPrice(10000000);
+
+        }else{ //최종 상품금액 천단위 절사
+            int settlePrice = productAmountResponse.getOriginPrice() - productAmountResponse.getDiscountPrice();
+            settlePrice = Math.round(settlePrice/10000)*10000;
+            productAmountResponse.setFinalPrice(settlePrice);
+        }
+
+        return productAmountResponse;
+    }//getProductAmount 종료
+
 }
