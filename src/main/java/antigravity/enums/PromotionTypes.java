f2e03226bd53d86809a0ab19e3b49d49a95c7dc5
ohjiyeon3275 <ojy940716@gmail.com>
2023-06-12 02:22:57 +0900

    feat : 주어진 param으로 쿠폰과 할인코드를 적용하여 가격 조회 api 구현 - 기본 요구사항

---
+++ src/main/java/antigravity/enums/PromotionTypes.java
@@ -0,0 +1,9 @@
+package antigravity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PromotionTypes {
+    COUPON,
+    CODE
+}
--- src/main/java/antigravity/service/ProductService.java
+++ src/main/java/antigravity/service/ProductService.java
@@ -1,22 +1,51 @@
 package antigravity.service;
 
 import antigravity.domain.entity.Product;
+import antigravity.domain.entity.Promotion;
+import antigravity.enums.PromotionTypes;
 import antigravity.model.request.ProductInfoRequest;
 import antigravity.model.response.ProductAmountResponse;
 import antigravity.repository.ProductRepository;
+import antigravity.repository.PromotionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @RequiredArgsConstructor
 @Service
 public class ProductService {
-    private final ProductRepository repository;
+    private final ProductRepository productRepository;
+    private final PromotionRepository promotionRepository;
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request) {
-        System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
 
-        Product product = repository.getProduct(request.getProductId());
+        Product product = productRepository.getProduct(request.getProductId());
+        int price = product.getPrice();
 
-        return null;
+        for(int couponId : request.getCouponIds()) {
+
+            Promotion promotion = promotionRepository.getPromotion(couponId);
+
+            String promotionName = promotion.getPromotion_type();
+            int discountValue = promotion.getDiscount_value();
+
+            PromotionTypes types = PromotionTypes.valueOf(promotionName);
+            switch (types) {
+                case COUPON:
+                    price -= discountValue;
+                    break;
+                case CODE:
+                    price -= (price * discountValue / 100);
+                    break;
+                default:
+            }
+        }
+
+        return ProductAmountResponse.builder()
+                .name(product.getName())
+                .originPrice(product.getPrice())
+                .discountPrice(product.getPrice() - price)
+                .finalPrice(price)
+                .build();
     }
 }
