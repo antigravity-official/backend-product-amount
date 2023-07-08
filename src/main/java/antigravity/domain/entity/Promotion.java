615f8500dd79fb2bd2e1aa545a0a8edd24f406aa
jongyounlee <jayylee.dev@gmail.com>
2023-07-09 01:35:20 +0900

    fix: snakeCase에서 camelCase로 변경

--- src/main/java/antigravity/domain/entity/Promotion.java
+++ src/main/java/antigravity/domain/entity/Promotion.java
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 @Builder
 public class Promotion {
     private Integer id;
-    private PromotionType promotion_type; //쿠폰 타입 (쿠폰, 코드)
+    private PromotionType promotionType; //쿠폰 타입 (쿠폰, 코드)
     private String name;
-    private DiscountType discount_type; // WON : 금액 할인, PERCENT : %할인
-    private Money discount_value; // 할인 금액 or 할인 %
-    private LocalDateTime use_started_at; // 쿠폰 사용가능 시작 기간
-    private LocalDateTime use_ended_at; // 쿠폰 사용가능 종료 기간
+    private DiscountType discountType; // WON : 금액 할인, PERCENT : %할인
+    private Money discountValue; // 할인 금액 or 할인 %
+    private LocalDateTime useStartedAt; // 쿠폰 사용가능 시작 기간
+    private LocalDateTime useEndedAt; // 쿠폰 사용가능 종료 기간
 }
