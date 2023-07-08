c4477ee25d5ab6b657fb4d1bf361c00443958789
jongyounlee <jayylee.dev@gmail.com>
2023-07-09 01:24:43 +0900

    fix: Date타입 LocalDateTime으로 변경

--- src/main/java/antigravity/domain/entity/Promotion.java
+++ src/main/java/antigravity/domain/entity/Promotion.java
@@ -3,6 +3,7 @@ package antigravity.domain.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -13,6 +14,6 @@ public class Promotion {
     private String name;
     private String discount_type; // WON : 금액 할인, PERCENT : %할인
     private Money discount_value; // 할인 금액 or 할인 %
-    private Date use_started_at; // 쿠폰 사용가능 시작 기간
-    private Date use_ended_at; // 쿠폰 사용가능 종료 기간
+    private LocalDateTime use_started_at; // 쿠폰 사용가능 시작 기간
+    private LocalDateTime use_ended_at; // 쿠폰 사용가능 종료 기간
 }
