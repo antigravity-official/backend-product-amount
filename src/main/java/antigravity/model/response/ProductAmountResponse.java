63810e647855a27b492afa57002b67c379ebcac5
keemsora <itsyoursora@gmail.com>
2023-12-02 22:40:10 +0900

    객체 생성을 위한 static 메서드 추가

--- src/main/java/antigravity/model/response/ProductAmountResponse.java
+++ src/main/java/antigravity/model/response/ProductAmountResponse.java
@@ -6,9 +6,19 @@ import lombok.Data;
 @Data
 @Builder
 public class ProductAmountResponse {
-    private String name; //상품명
 
+    private String name; //상품명
     private int originPrice; //상품 기존 가격
     private int discountPrice; //총 할인 금액
     private int finalPrice; //확정 상품 가격
+
+    public static ProductAmountResponse from(String name, int originPrice,
+                                            int discountPrice, int finalPrice) {
+        return new ProductAmountResponse(
+            name,
+            originPrice,
+            discountPrice,
+            finalPrice
+        );
+    }
 }
